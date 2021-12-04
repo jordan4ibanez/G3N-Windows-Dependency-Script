09351d3dbab4b9f3f5751d6103737826f40c21a9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-04 17:07:38 -0500

    Automate building of G3N and make code return to old directory
    This also temporarily adds the mingw-w64 bin to the environment path

--- G3N-dependencies.ps1
+++ G3N-dependencies.ps1
@@ -4,7 +4,7 @@ $currentPath = Get-Location
 #getting user confirmation so they do not accidentally put this somewhere crazy like C:\Windows\sysWOW64\
 
 Write-Host "This is designed to be run where you wish to store mingw-w64 for building. PLEASE ensure you are in the correct directory ($currentPath) before continuing."
-Write-Host "Requirements are: 7zip (default install path), Git (default install path)"
+Write-Host "Requirements are: 7zip (default install path), Git (default install path), Go"
 
 $userConfirmation = Read-Host "Please type YES (caps included) if you wish to continue"
 
@@ -54,18 +54,29 @@ if ((Test-Path -Path "C:\Program Files\Git\") -and (Test-Path -Path "C:\Program 
     Exit -1
 }
 
-
-
 Write-Host "Grabbing mingw-w64 toolchain (7z formatted)..."
 
 #Yes, this is a very long address - this is also probably not the fastest around the world - but I can only pick the fastest that I can find for my location from sourceforge
-#Invoke-WebRequest -uri "https://versaweb.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z" -Outfile mingw-toolchain.7Z
+Invoke-WebRequest -uri "https://versaweb.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z" -Outfile mingw-toolchain.7Z
 
 Write-Host "Unzipping mingw-w64 toolchain..."
 
 set-alias sz "C:\Program Files\7-Zip\7z.exe"
 
-#Invoke-Expression "sz x $currentPath\mingw-toolchain.7Z -o$currentPath\mingw-w64\ -y"
+Invoke-Expression "sz x $currentPath\mingw-toolchain.7Z -o$currentPath\mingw-w64\ -y"
 
+Write-Host "mingw-w64 extraction successful. Temporarily adding to system path."
 
+$env:Path += ";$currentPath\mingw-w64\mingw64\bin\"
+
+Write-Host "Cloning G3N from Github..."
+
+Invoke-Expression "git clone https://github.com/g3n/engine g3n-engine"
+
+Invoke-Expression "cd g3n-engine"
+
+Invoke-Expression "go install ./..."
+
+#this takes us out of the go build directory
+Invoke-Expression "cd .."
 
